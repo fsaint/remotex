441f48fc5014e500945974f68daef79d9e6c6534
Felipe Saint-Jean <fsaint@gmail.com>
2026-05-03 16:54:59 -0700

    feat: KeychainStore for saving/loading Credentials

---
+++ ios/RemoteXTests/KeychainStoreTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+@testable import RemoteX
+
+final class KeychainStoreTests: XCTestCase {
+    let store = KeychainStore()
+
+    override func setUp() {
+        super.setUp()
+        store.deleteCredentials()
+    }
+
+    func testSaveAndLoad() throws {
+        let creds = Credentials(
+            host: "myhost.ts.net",
+            apiKey: "test-api-key-123",
+            sshPrivateKey: "-----BEGIN OPENSSH PRIVATE KEY-----\ntest\n-----END OPENSSH PRIVATE KEY-----"
+        )
+        XCTAssertNoThrow(try store.save(creds))
+
+        let loaded = try store.load()
+        XCTAssertEqual(loaded.host, creds.host)
+        XCTAssertEqual(loaded.apiKey, creds.apiKey)
+        XCTAssertEqual(loaded.sshPrivateKey, creds.sshPrivateKey)
+    }
+
+    func testLoadThrowsWhenEmpty() {
+        XCTAssertThrowsError(try store.load()) { error in
+            XCTAssertEqual(error as? KeychainStore.Error, .notFound)
+        }
+    }
+
+    func testIsPaired() throws {
+        XCTAssertFalse(store.isPaired)
+        try store.save(Credentials(host: "h", apiKey: "k", sshPrivateKey: "p"))
+        XCTAssertTrue(store.isPaired)
+    }
+}
